91f101dd7b2ef111703f8825465fd5f7f4d5c9f4
Frank Gerlings <f.gerlings@student.science.ru.nl>
2020-04-17 17:53:54 +0200

    Finished ex1

--- 05-ensemble-learning/ex1_scripts.py
+++ 05-ensemble-learning/ex1_scripts.py
@@ -1,23 +1,38 @@
-import math as m
+import math
+
+w, h = 40, 40
+lookupTable = [[0.0 for x in range(w)] for y in range(h)]
 
 
-def binomialCoeff(n, k):
+def binom_lookup_table(n, k):
+    if lookupTable[n][k] == 0.0:
+        # Compute the new value
+        coeff = binomial_coeff(n, k)
+        lookupTable[n][k] = coeff
+        return coeff
+    else:
+        # Lookup the old value
+        return lookupTable[n][k]
+
+
+def binomial_coeff(n, k):
+    # n choose k
     if k == 0 or k == n:
         return 1
     # Recursive Call
-    return binomialCoeff(n - 1, k - 1) + binomialCoeff(n - 1, k)
+    return binomial_coeff(n - 1, k - 1) + binomial_coeff(n - 1, k)
 
-# 0 <= p <= 1
-# n > 0, odd if you want sensible answers
+
 def chance_of_success(p, n):
+    # 0 <= p <= 1
+    # n > 0, odd if you want sensible answers
     aggregated_chance = 0
-    for i in range(0, m.ceil(n/2) + 1):
-        aggregated_chance += binomialCoeff(n, i) * pow(p, n - i) * pow(1-p, n-i)
-        print(str(i) + ": " + str(aggregated_chance))
+    for i in range(0, math.ceil(n/2)):
+        aggregated_chance += binom_lookup_table(n, i) * pow(p, n - i) * pow(1 - p, i)
     return aggregated_chance
 
 
-print(binomialCoeff(3, 2))
-print("Result: ")
-print(chance_of_success(0.8, 3))
+for i in range(0, 10):
+    print(chance_of_success(0.85, i*2+1))
+
 # print(chance_of_success(0.5, 2))
