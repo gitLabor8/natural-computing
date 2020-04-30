Attempt to correct 03s auc calculation

03-negative-selection/negative-selection/negative_selection.py
@@ -3,37 +3,51 @@ import numpy as np
 from sklearn import metrics
 import subprocess
 import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
 
 # n = pattern length
 # r = matched substring length
-def run_negative_selection(n, r, filepath):
-    output = subprocess.check_output("java -jar negsel2.jar -self english.train -n " + str(n) + " -r " + str(r) + " -c -l < english.test" , shell=True)
+def run_negative_selection(n, r, foreign_filepath):
+    output = subprocess.check_output("java -jar negsel2.jar -self english.train -n "
+                + str(n) + " -r " + str(r) + " -c -l < english.test", shell=True)
     output = np.asarray(output.splitlines())
     english_test = [float(data.decode("utf-8")) for data in output]
+    print("english_test = " + str(english_test))
 
-    output = subprocess.check_output("java -jar negsel2.jar -self english.train -n " + str(n) + " -r " + str(r) + " -c -l < " +filepath, shell=True)
+    output = subprocess.check_output("java -jar negsel2.jar -self english.train -n "
+                + str(n) + " -r " + str(r) + " -c -l < " + foreign_filepath, shell=True)
     output = np.asarray(output.splitlines())
-    tagalog_test = [float(data.decode("utf-8")) for data in output]
+    foreign_test = [float(data.decode("utf-8")) for data in output]
+    print("other_output = " + str(foreign_test))
 
-    return np.asarray(english_test), np.asarray(tagalog_test)
+    return np.asarray(english_test), np.asarray(foreign_test)
 
-
+# Old auc calculation, just here for reference
 def calc_auc(english_test, foreign_test, threshold):
     y_true1 = np.ones(english_test.size)
     y_true0 = np.zeros(foreign_test.size)
     y_true = np.concatenate((y_true1, y_true0)) != 0
 
     y_pred = np.concatenate((english_test, foreign_test))
-    y_pred = np.where(y_pred>threshold, 0, 1)
+    y_pred = np.where(y_pred, 0, 1)
 
     fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred)
-    # plt.plot(fpr, tpr)
-    # plt.title("ROC Curve")
-    # plt.xlabel("False Positive Rate")
-    # plt.ylabel("True Positive Rate")
-    # plt.show()
     return metrics.auc(fpr, tpr)
 
+# New auc calculation filled with feedback help from the assignment correction
+def new_auc(english_test, foreign_test):
+    y_true1 = np.ones(english_test.size)
+    y_true0 = np.zeros(foreign_test.size)
+    y_true = np.concatenate((y_true1, y_true0)) != 0  # labels  # contains 0 for
+    # English lines, 1 for other language lines
+    y_pred = np.concatenate((english_test, foreign_test))
+    # y_pred = np.where(y_pred, 0, 1) # scores  # list containing scores for the
+    # different English and other Language lines
+    fpr, tpr, _ = roc_curve(y_true, y_pred)  # roc function can directly handle
+    # the negative selection anomaly scores, no need to binarize the scores or loop through thresholds yourself
+    return auc(fpr, tpr)  # contains the AUC for this dataset
+    # plt.plot(fpr, tpr)  # optionally plot ROC curve, with matplotlib
 
 def main():
     filepaths = ["lang/hiligaynon.txt", "lang/middle-english.txt",
@@ -41,7 +55,7 @@ def main():
     for filepath in filepaths:
         english_test, foreign_test = run_negative_selection(10, 4, filepath)
         threshold = np.mean([*english_test, *foreign_test])
-        print((filepath, str(calc_auc(english_test,foreign_test,threshold))))
+        print((filepath, str(new_auc(english_test, foreign_test))))
 
     # Results for other languages, with r=4
     # ('lang/hiligaynon.txt', '0.7624516129032258')
@@ -51,13 +65,11 @@ def main():
 
 
 def find_r():
-    for r in range(1, 10):
+    for r in range(4, 5):
         english_test, tagalog_test = run_negative_selection(10, r, "tagalog.test")
-        # print(np.mean(english_test))
-        # print(np.mean(tagalog_test))
         threshold = np.mean([*english_test, *tagalog_test])
-        # print("threshold ="+str(threshold))
-        auc = calc_auc(english_test, tagalog_test, threshold)
+        print("new auch?")
+        auc = new_auc(english_test, tagalog_test)
         print((str(r), str(auc)))
 
     # Results:
@@ -73,4 +85,12 @@ def find_r():
 
 
 # find_r()
-main()
+# main()
+
+# Manual testing: this should yield "about 0.79"
+output = subprocess.check_output("java -jar negsel2.jar -self english.train -n "
+            + str(10) + " -r " + str(4) + " -c -l < english.test", shell=True)
+output = np.asarray(output.splitlines())
+english_test = [float(data.decode("utf-8")) for data in output]
+print("english_test = " + str(english_test))
+