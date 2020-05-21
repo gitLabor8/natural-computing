1078cb47b12ff3dae81a85dee40ce04c878e5ae4
Frank Gerlings <f.gerlings@student.science.ru.nl>
2020-05-21 10:22:38 +0200

    stash

--- project_GIRP/code/driver.py
+++ project_GIRP/code/driver.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 # Class that creates an instance of GIRP and plays games in it.
 
-import sys, string, os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 class Driver:
     def __init__(self):
-        # TODO distinguish between Mac and Windows
         # Start executing GIRP.exe
-        os.system("./GIRP.exe")
+
+        driver = webdriver.Chrome("./selenium/chromedriver.exe")
+        driver.get("http://www.foddy.net/GIRP.html")
+        driver.implicitly_wait(1)
+        assert "GIRP" in driver.title
+
+        driver.close()
 
     # Given a sequence plays the game and returns the achieved high score
     def play_game(self, codeSequence):
--- project_GIRP/code/main.py
+++ project_GIRP/code/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Top level executing file
 
-from .driver import Driver
+from driver import Driver
 
 driver = Driver()
