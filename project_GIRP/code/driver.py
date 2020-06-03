160d51a812865c66d99ecfb5734775209e82005a
Bart van den Boom <bartboom@Barts-MacBook-Air.local>
2020-06-03 18:17:38 +0200

    Made the controller (former driver) and Fitness multithreaded such that the score can be captured in regular intervals during the run

--- project_GIRP/code/driver.py
+++ project_GIRP/code/driver.py
@@ -11,6 +11,7 @@ import matplotlib
 import pylab as plt
 import cv2
 import re
+import threading
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -18,37 +19,25 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 # Indicates whether a key is currently pressed.
 KEYBOARD = {'a':False, 'b':False, 'c':False, 'd':False, 'e':False, 'f':False, 'g':False, 'h':False, 'i':False, 'j':False, 'k':False, 'l':False, 'm':False, 'n':False, 'o':False, 'p':False, 'q':False, 'r':False, 's':False, 't':False, 'u':False, 'v':False, 'w':False, 'x':False, 'y':False, 'z':False, 'shift':False}
-ACTIONS = {} #Actions which result from decoding the genetic code.
-class Driver:
-    def __init__(self):
-        self.browser = None
-        self.start_firefox()
-        self.visit_GIRP()
-        self.start_game(self.get_GIRP_element())
-        self.busy = False
-        print("Driver initialized.")
 
+def delay(t):
+    time.sleep(t/1000)
+
+def get_GIRP_element(browser):
+    return browser.find_element_by_css_selector("div[class=post-body]")
+
+class Fitness:
+    def __init__(self, driver):
+        self.score = list()
+        self.alive = True
+        self.browser = driver
     def __del__(self):
-        if self.browser:
-            self.browser.quit()
+        pass
 
-    def delay(self, t):
-        time.sleep(t/1000)
-
-    # Given a sequence plays the game and returns the fitness
-    def play_game(self, codeSequence):
-        # TODO
-        if not self.busy:
-            print("Start new candidate solution.")
-            fitness = self.controller(codeSequence)
-        else:
-            print("Controller busy.")
-
-    #FROM: https://stackoverflow.com/questions/15018372/how-to-take-partial-screenshot-with-selenium-webdriver-in-python
-    def capture_element(self, element, driver):
+    def capture_score_img(self, element):
         location = element.location
         size = element.size
-        img = driver.get_screenshot_as_png()
+        img = self.browser.get_screenshot_as_png()
         img = Image.open(io.BytesIO(img))
         left = location['x']
         top = location['y']
@@ -57,23 +46,6 @@ class Driver:
         new_left = left + ((size['width']-640)/2)
         new_right = right - ((size['width']-640)/2)
         new_bottom = location['y'] + 480
-        img = img.crop((int(new_left), int(top), int(new_right), int(new_bottom)))
-        # img.save('screenshot.png') #This is a test
-        #return img
-
-    def capture_score_img(self, element, driver):
-        location = element.location
-        size = element.size
-        img = driver.get_screenshot_as_png()
-        img = Image.open(io.BytesIO(img))
-        left = location['x']
-        top = location['y']
-        right = location['x'] + size['width'] #Actual size is 640*480
-        bottom = location['y'] + size['height']
-        new_left = left + ((size['width']-640)/2)
-        new_right = right - ((size['width']-640)/2)
-        new_bottom = location['y'] + 480
-
         score_bottom = new_bottom - 30
         score_top = score_bottom - 40
         score_right = new_left + 110 #Increase for fourth character
@@ -82,47 +54,6 @@ class Driver:
         img = img.crop((int(score_left), int(score_top), int(score_right), int(score_bottom)))
         img = np.asarray(img)
         return img
-
-    def start_chrome(self): # Not used.
-        options = webdriver.ChromeOptions()
-        prefs = {
-            "profile.default_content_setting_values.plugins": 1,
-            "profile.content_settings.plugin_whitelist.adobe-flash-player": 1,
-            "profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player": 1,
-            "profile.content_settings.exceptions.plugins.*,*.setting": 1
-        }
-
-        options.add_experimental_option("prefs", prefs)
-        options.add_argument('--disable-features=EnableEphemeralFlashPermission')
-        options.add_argument('--disable-infobars')
-        options.add_argument("--ppapi-flash-version=32.0.0.101")
-        options.add_argument("--ppapi-flash-path=/usr/lib/pepperflashplugin-nonfree/libpepflashplayer.so")
-        options.add_experimental_option("prefs",prefs)
-        self.browser = webdriver.Chrome(chrome_options=options)
-        print("Chrome driver started.")
-
-    def start_firefox(self):
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("dom.ipc.plugins.enabled.libflashplayer.so","true")
-        fp.set_preference("plugin.state.flash", 2)
-        self.browser = webdriver.Firefox(fp)
-        print("Firefox driver started")
-
-    def visit_GIRP(self):
-        print("Visiting GIRP URL.")
-        self.browser.get('http://www.foddy.net/GIRP.html')
-        time.sleep(1)
-
-    def get_GIRP_element(self):
-        return self.browser.find_element_by_css_selector("div[class=post-body]")
-
-    def start_game(self, element):
-        ac = ActionChains(self.browser)
-        ac.move_to_element(element).click().perform() # Click to activate the flash player
-        print("Manually Click allow.")
-        self.delay(5000)
-        print("Manually the game and start.")
-        self.delay(2000)
 
     def mean_squeared_error(self, imageA, imageB):
         err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
@@ -143,7 +74,7 @@ class Driver:
             return -1
 
     def get_score(self):
-        score_img = self.capture_score_img(self.get_GIRP_element(), self.browser)
+        score_img = self.capture_score_img(get_GIRP_element(self.browser))
         score_img = ~(np.array(score_img)[:,:,0]) #Removes rgb and inverts colors
         score_img = cv2.threshold(score_img, 20, 255, cv2.THRESH_BINARY)[1] #threshold to remove color artifacts and leave it black and white
         first_digit = self.get_digit(score_img[:,:28])
@@ -152,32 +83,94 @@ class Driver:
         try:
             return float(first_digit+second_digit+third_digit)
         except:
-            return "err"
+            return -1.0
+
+    def capture_score(self):
+        print("Score Thread started.")
+        score = list()
+        game_started = False
+        while self.alive:
+            height = self.get_score()
+            score.append(height)
+            delay(300)
+            if height > 0.0:
+                game_started = True
+            if game_started and height == 0.0:
+                self.alive = False
+            print(height)
+        print("Score Thread finished.")
+        return score
+
+class Controller:
+    def __init__(self):
+        self.browser = self.start_firefox()
+        self.visit_GIRP()
+        self.start_game(get_GIRP_element(self.browser))
+        self.busy = False
+        self.alive = False
+        print("Driver initialized.")
+
+    def __del__(self):
+        if self.browser:
+            self.browser.quit()
+
+    def get_driver(self):
+        return self.browser
+
+    def play_game(self, codeSequence):
+        # TODO
+        print("Game controller Thread started.")
+        if not self.busy:
+            print("Start run.")
+            self.controller(codeSequence)
+        else:
+            print("Busy.")
+        print("Game controller Thread finished.")
+
+    def start_firefox(self):
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("dom.ipc.plugins.enabled.libflashplayer.so","true")
+        fp.set_preference("plugin.state.flash", 2)
+        return webdriver.Firefox(fp)
+        print("Firefox driver started")
+
+    def visit_GIRP(self):
+        print("Visiting GIRP URL.")
+        self.browser.get('http://www.foddy.net/GIRP.html')
+        time.sleep(1)
+
+    def start_game(self, element):
+        ac = ActionChains(self.browser)
+        ac.move_to_element(element).click().perform() # Click to activate the flash player
+        print("Manually Click allow.")
+        delay(5000)
+        print("Manually the game and start.")
+        delay(2000)
 
     def key_press(self, a):
         ## TODO:
         if a in KEYBOARD:
-            print("Press %s" % a)
             keyboard.send(a, do_press=True, do_release=False)
             KEYBOARD[a] = True
 
     def key_release(self, a):
         ## TODO:
         if KEYBOARD[a]:
-            print("Release %s" % a)
             keyboard.send(a, do_press=False, do_release=True)
             KEYBOARD[a] = False
 
     def controller(self, gene):
         ## TODO: return fitness
         self.busy = True
+        self.alive = True
         for action in gene:
             if action == "+":
+                self.key_press('shift')
                 self.key_press('shift')
             elif action == "-":
                 self.key_release('shift')
             elif action == ".":
-                self.delay(200)
+                delay(300)
             elif action.isupper():
                 self.key_press(action.lower())
             elif action.islower():
@@ -185,4 +178,3 @@ class Driver:
             else:
                 print("Illegal action.")
         self.busy = False
-        return 0
--- project_GIRP/code/main.py
+++ project_GIRP/code/main.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 # Top level executing file
 
-from driver import Driver
+from driver import Controller, Fitness
+import threading
+from concurrent.futures import ThreadPoolExecutor
 
-driver = Driver()
 
 from gene import Gene
 
-gene1 = Gene("abcdefgh", "a", 6)
-gene2 = Gene("abcdefgh", "a", 6)
-print("The parents:")
-print(str(gene1))
-print(str(gene2))
+controller = Controller()
+fitness = Fitness(controller.get_driver())
 
-driver.play_game(gene1.button_press_encoding())
+# executor = ThreadPoolExecutor(max_workers=2)
+
+# gene2 = Gene("abcdefgh", "a", 6)
+# print("The parents:")
+# print(gene1.button_press_encoding())
+# print(str(gene2))
+
+# driver.play_game(gene1.button_press_encoding())
+# controller.play_game("M......F+.......-....m...D+....-....")
+codeSequence = "M......F+.......-....m...D+....-...."
+
+threading.Thread(target=controller.play_game, args=(codeSequence,)).start()
+threading.Thread(target=fitness.capture_score, args=()).start()
+
+# game = executor.submit(driver.play_game("M......F+.......-....m...D+....-...."))d
+# score = executor.submit(driver.capture_score())
+# ret = score.result()
+# print(ret)
 
 # # Important! Make a copy of a parent
 # geneChild = gene1
